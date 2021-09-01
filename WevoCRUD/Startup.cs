eba0328c3da162c058b4552d4d70a9d5cbfbd137
Edson Soares <edsondearaujofga@gmail.com>
2021-09-01 19:24:28 -0300

    Add services

--- WevoCRUD/Startup.cs
+++ WevoCRUD/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WevoCRUD.Data;
+using WevoCRUD.Repositories;
 
 namespace WevoCRUD
 {
@@ -24,7 +26,10 @@ namespace WevoCRUD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IUserContext, UserContext>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddControllers();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
